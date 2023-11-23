a96b9f5f4d09df3de7c4054c59e117b2a7e06e99
Jackie Blunt <jblunt10@gmail.com>
2023-11-23 11:58:01 -0800

    updated app to include citation

--- app.R
+++ app.R
@@ -1,22 +1,42 @@
 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-penguins <- read.csv("penguins.csv", stringsAsFactors = FALSE)
+library(dplyr)
+penguins <- read.csv("https://raw.githubusercontent.com/stat545ubc-2023/assignment-b3-jacquelineb174/main/penguins.csv", stringsAsFactors = FALSE)
+
 
 ui <- fluidPage(
-  titlePanel("Find the Right Penguin!"),
+  titlePanel("Find the Right Penguin!"), "Welcome! This app is designed to help you find the IDs of penguins from the palmerpenguins dataset that fit the qualities you are looking for.",
+  br(), "The palmerpenguins github is here", a("https://github.com/allisonhorst/palmerpenguins"),
+#Feature #1 is adding a layout. This is useful because I can separate the side bar to be where the user adjusts the variable and have a table populate in the main panel beside rather than below. 
 sidebarLayout(
-  sidebarPanel(sliderInput("flipperInput", "Flipper Length", min = 0, max = 100,
-                           value = c(25, 40), post = "mm"), 
-               radioButtons("speciesInput", "Species",
-                            choices = c("Adelie", "Gentoo", "Chinstrap"),
-                            selected = "Adelie")),
-  mainPanel("the results will go here")
-  )
-)
-server <- function(input, output) {}
+  sidebarPanel(
+#Feature #2 is adding interactive selection buttons and sliders. This is useful so that the user can enter a few qualities they are looking for to find only penguins that fit their criteria. 
+    sliderInput("massInput", "Body Mass (g)", min = 2650, max = 6350,
+                 value = c(3390, 5000), post = "mm"), 
+    radioButtons("speciesInput", "Species",
+                 choices = c("Adelie", "Gentoo", "Chinstrap"),
+                 selected = "Adelie"),
+    selectInput("sexInput", "Sex",
+                 choices = c("female", "male"))
+               ),
+  mainPanel(tableOutput("penguinids"))
+             )
+         )
 
+#within fluid page put citation to dataset in actual letters that show up in app and reference in readme 
+
+server <- function(input, output) { 
+  filtered <- reactive({
+    penguins %>%
+      filter(body_mass_g >= input$massInput[1],
+             body_mass_g <= input$massInput[2],
+             species == input$speciesInput,
+             sex == input$sexInput)
+  })
+#Feature #3 is an ouput table. This is useful as it provides the user with a list of penguins that fit the criteria that have searched for.   
+  output$penguinids <- renderTable({
+    filtered()
+  })
+}
 
 shinyApp(ui = ui, server = server)
