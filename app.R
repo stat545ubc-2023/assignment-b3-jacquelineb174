bb8adddc8f89cfa0e1841cab009eb9587c8c3995
Jackie Blunt <jblunt10@gmail.com>
2023-11-23 12:12:59 -0800

    update citation

--- app.R
+++ app.R
@@ -7,10 +7,10 @@ penguins <- read.csv("https://raw.githubusercontent.com/stat545ubc-2023/assignme
 ui <- fluidPage(
   titlePanel("Find the Right Penguin!"), "Welcome! This app is designed to help you find the IDs of penguins from the palmerpenguins dataset that fit the qualities you are looking for.",
   br(), "The palmerpenguins github is here", a("https://github.com/allisonhorst/palmerpenguins"),
-#Feature #1 is adding a layout. This is useful because I can separate the side bar to be where the user adjusts the variable and have a table populate in the main panel beside rather than below. 
+#Feature #1 is adding a layout. This is useful because I can separate the side bar to contain the variables to be adjusted by the user and have a separate table populate in the main panel beside. 
 sidebarLayout(
   sidebarPanel(
-#Feature #2 is adding interactive selection buttons and sliders. This is useful so that the user can enter a few qualities they are looking for to find only penguins that fit their criteria. 
+#Feature #2 is adding interactive selection buttons and sliders. This is useful so that the user can enter a few qualities they are looking for to get a list of penguins that fit their criteria. 
     sliderInput("massInput", "Body Mass (g)", min = 2650, max = 6350,
                  value = c(3390, 5000), post = "mm"), 
     radioButtons("speciesInput", "Species",
@@ -23,8 +23,6 @@ sidebarLayout(
              )
          )
 
-#within fluid page put citation to dataset in actual letters that show up in app and reference in readme 
-
 server <- function(input, output) { 
   filtered <- reactive({
     penguins %>%
@@ -33,7 +31,7 @@ server <- function(input, output) {
              species == input$speciesInput,
              sex == input$sexInput)
   })
-#Feature #3 is an ouput table. This is useful as it provides the user with a list of penguins that fit the criteria that have searched for.   
+#Feature #3 is an output table. This is useful as it provides the user with a list of penguins that fit the criteria that have searched for.   
   output$penguinids <- renderTable({
     filtered()
   })
