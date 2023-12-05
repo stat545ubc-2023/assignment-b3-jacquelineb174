b0529e25457a67cbe5ed3278a583cff6be0975b9
Jackie Blunt <jblunt10@gmail.com>
2023-12-05 12:44:14 -0800

    Image, download button, theme

--- app.R
+++ app.R
@@ -1,26 +1,31 @@
 library(shiny)
 library(dplyr)
+library(shinythemes)
 penguins <- read.csv("https://raw.githubusercontent.com/stat545ubc-2023/assignment-b3-jacquelineb174/main/penguins.csv", stringsAsFactors = FALSE)
 
 
 ui <- fluidPage(
+  #New feature #1 is adding a theme. Although it does not serve a functional roles, adding a theme is useful as it makes the site more visually appealing. 
+  theme = shinytheme("superhero"),
   titlePanel("Find the Right Penguin!"), "Welcome! This app is designed to help you find the IDs of penguins from the palmerpenguins dataset that fit the qualities you are looking for.",
-  br(), "The palmerpenguins github is", a("here", href = "https://github.com/allisonhorst/palmerpenguins"),
-  #Feature #1 is adding a layout. This is useful because I can separate the side bar to contain the variables to be adjusted by the user and have a separate table populate in the main panel beside. 
+  br(), " Use the buttons  below to select the penguin's qualities you would like. ", br(), "The palmerpenguins github is", a("here", href = "https://github.com/allisonhorst/palmerpenguins"),
   sidebarLayout(
     sidebarPanel(
-      #Feature #2 is adding interactive selection buttons and sliders. This is useful so that the user can enter a few qualities they are looking for to get a list of penguins that fit their criteria. 
       sliderInput("massInput", "Body Mass (g)", min = 2650, max = 6350,
-                  value = c(3390, 5000), post = "mm"), 
+                  value = c(3390, 5000), post = "g"), 
       radioButtons("speciesInput", "Species",
-                   choices = c("Adelie", "Gentoo", "Chinstrap"),
-                   selected = "Adelie"),
-      selectInput("sexInput", "Sex",
-                  choices = c("female", "male"))
-    ),
-    mainPanel(tableOutput("penguinids"))
-  )
-)
+                   choices = c("Adelie", "Gentoo", "Chinstrap")),
+      radioButtons("sexInput", "Sex",
+                  choices = c("female", "male")),
+      #New feature #2 is to add an image of the "palmer penguins". This is useful as it makes the app more sightly and also has labels that help distinguish between the three species of penguin.
+      img(src = "penguins2.png", height = 220, width = 400), br(), 
+      "Image created by", a("Allison Horst", href = "https://allisonhorst.github.io/palmerpenguins/articles/intro.html")
+                ),
+    #New feature #3 is to add a download button.This is useful as it allows the user to download the output table of the penguins that match their description. 
+    mainPanel(downloadButton("PenguinMatch", "Download table here"), 
+              tableOutput("penguinids"))
+              )
+            )
 
 server <- function(input, output) { 
   filtered <- reactive({
@@ -30,11 +35,17 @@ server <- function(input, output) {
              species == input$speciesInput,
              sex == input$sexInput)
   })
-  #Feature #3 is an output table. This is useful as it provides the user with a list of penguins that fit the criteria that have searched for.   
   output$penguinids <- renderTable({
     filtered()
   })
+  output$PenguinMatch <- downloadHandler(
+    filename = function() {
+      paste(PenguinMatch, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered(), file, row.names = FALSE)
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
-
